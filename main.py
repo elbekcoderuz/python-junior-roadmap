4f0faee42793e9d97932051de1b2bc048daed94b
elbekcoderuz <126439702+elbekcoderuz@users.noreply.github.com>
2023-03-07 11:10:35 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# Brinchi 
+a = int(input("Sonni kiriting: "))
+if (a % 3 == 0 and a % 5 == 0):
+    print("Fizz Buzz")
+elif (a % 3 == 0):
+    print("Fizz")
+elif (a % 5 == 0):
+    print("Buzz")
+else:
+    print("Xatolik!")
+
+#Ikinchisi
+login = input("loginni kiriting")
+email = input("emailni kiriting")
+parol = input("parolni kiriting")
+
+login == 2009
+email == "elbekxomitov828@gmail.com"
+parol == 20092012
+
+if(login == 2009 and email == "elbekxomitov828@gmail.com" and parol == 20092012 ):
+    print("Admin: xush kelibsiz")
+
+#uchinchi 
+print = ("Maxanizatsiyalashtirishmoqchiliginiaytganinibilibqoldim") [0:13]
+print = ("Maxanizatsiyalashtirishmoqchiliginiaytganinibilibqoldim") [0:53]
+
+#To`rtinchi 
+a = int(input("1-sonni kiriting:"))
+b = int(input("2-sonni kiriting:"))
+
+
+c = a + b 
+d = a - b 
+e = a * b
+f = a / b 
+print(c, d, e, f, sep="=> ") 
+
